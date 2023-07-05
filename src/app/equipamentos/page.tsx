94122d667281f03617fc419660eb257b3a4eb16a
Kyoudan <fogoste@gmail.com>
2023-07-05 18:11:47 -0300

    feat: creating equipment page structure

---
+++ src/app/equipamentos/page.tsx
@@ -0,0 +1,12 @@
+import { FooterComponent } from "@/components/global/Footer";
+import { HeaderComponent } from "@/components/global/Header";
+
+export default function Equipmaentos() {
+    return (
+        <main className="overflow-x-hidden ">
+            <HeaderComponent />
+
+            <FooterComponent />
+        </main>
+    );
+}
--- src/components/global/Header/index.tsx
+++ src/components/global/Header/index.tsx
@@ -1,5 +1,6 @@
 import LogoIF from "@/assets/IfLogo.png";
 import Image from "next/image";
+import Link from "next/link";
 
 const buttonsStyle =
   "p-2 hover:bg-primary-light-100 transition-colors text-white rounded font-montserrat font-bold";
@@ -8,13 +9,13 @@ export const HeaderComponent = () => {
   return (
       <div className="w-screen bg-primary flex sm:flex-row flex-col items-center justify-between pt-2 pb-2 gap-6">
           <div className="ml-4">
-              <button>
+              <Link href="/">
                   <Image src={LogoIF} alt="logo" className="w-auto h-12" />
-              </button>
+              </Link>
           </div>
           <div className="sm:w-1/2 w-full flex flex-row items-center sm:justify-end justify-center gap-4 sm:mr-4 flex-wrap">
-              <button className={buttonsStyle}>Home</button>
-              <button className={buttonsStyle}>Equipamentos</button>
+              <Link href="/" className={buttonsStyle}>Home</Link>
+              <Link href="/equipamentos" className={buttonsStyle}>Equipamentos</Link>
               <button className={buttonsStyle}>Galeria</button>
               <button className={buttonsStyle}>Agendamento</button>
           </div>
