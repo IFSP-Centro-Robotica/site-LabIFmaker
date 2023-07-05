e57a5faa9eb86060cdb70defa067b999007f21c7
Kyoudan <fogoste@gmail.com>
2023-07-05 12:03:37 -0300

    fix: fixing navbar responsivide

--- src/components/global/Header/index.tsx
+++ src/components/global/Header/index.tsx
@@ -6,18 +6,18 @@ const buttonsStyle =
 
 export const HeaderComponent = () => {
   return (
-    <div className="w-screen bg-primary flex flex-row justify-between pt-2 pb-2">
-      <div className="ml-4">
-        <button>
-          <Image src={LogoIF} alt="logo" className="w-auto h-12" />
-        </button>
+      <div className="w-screen bg-primary flex sm:flex-row flex-col items-center justify-between pt-2 pb-2 gap-6">
+          <div className="ml-4">
+              <button>
+                  <Image src={LogoIF} alt="logo" className="w-auto h-12" />
+              </button>
+          </div>
+          <div className="sm:w-1/2 w-full flex flex-row items-center sm:justify-end justify-center gap-4 sm:mr-4 flex-wrap">
+              <button className={buttonsStyle}>Home</button>
+              <button className={buttonsStyle}>Equipamentos</button>
+              <button className={buttonsStyle}>Galeria</button>
+              <button className={buttonsStyle}>Agendamento</button>
+          </div>
       </div>
-      <div className="w-1/2 flex flex-row items-center justify-end gap-4 mr-4">
-        <button className={buttonsStyle}>Home</button>
-        <button className={buttonsStyle}>Equipamentos</button>
-        <button className={buttonsStyle}>Galeria</button>
-        <button className={buttonsStyle}>Agendamento</button>
-      </div>
-    </div>
   );
 };
