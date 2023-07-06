11884be8b0fdf91007737ca991a9e5489716266a
Kyoudan <fogoste@gmail.com>
2023-07-06 09:08:52 -0300

    feat: configuring equipment mock typing

--- src/components/pages/Equipamentos/equipment/index.tsx
+++ src/components/pages/Equipamentos/equipment/index.tsx
@@ -6,7 +6,7 @@ export const EquipamentEquipamentos = () => {
     <section className="w-full flex flex-row pr-4 pl-4 pt-8 pb-8 flex-wrap gap-8 items-center justify-center">
       {equipamentFields.map((equipament) => (
         <div
-          key={equipament.href}
+          key={equipament.slug}
           className="max-w-[380px] bg-zinc-100 p-6 rounded border border-spacing-1 border-primary transition flex flex-col gap-4 hover:shadow-2xl"
         >
           <h1 className="text-2xl font-montserrat font-bold text-primary">
@@ -17,7 +17,7 @@ export const EquipamentEquipamentos = () => {
             {equipament.description}
           </p>
           <Link
-            href={equipament.href}
+            href={`/equipamentos/${equipament.slug}`}
             className="bg-primary text-white rounded font-montserrat font-bold transition-colors hover:bg-primary-light-100 text-center p-3"
           >
             Acessar
--- src/mocks/equipaments/equipament.tsx
+++ src/mocks/equipaments/equipament.tsx
@@ -5,14 +5,47 @@ const equipamentStyle = "rounded border border-spacing-1 border-zinc-500";
 
 interface IEquipamentFields {
   title: string;
+  slug: string;
   image: () => JSX.Element;
   description: string;
-  href: string;
+  content?: [
+    {
+      type?: {
+        title: "Tipo: ";
+        type: string;
+      };
+      brand?: {
+        title: "Marca: ";
+        brand: string;
+      };
+      softwares?: {
+        title: "Softwares para utilização: ";
+        softwares: string;
+      };
+      extensions?: {
+        title: "Extenções de arquivos suportados pelo software proprietário deste equipamento: ";
+        extensions: string;
+      };
+      recommendedMaterialsTobeUsed?: {
+        title: "Matérias-primas recomendados para utilizar no equipamento: ";
+        recommendedMaterialsTobeUsed: string;
+      };
+      website?: {
+        title: "Para mais informações, visite o site do: ";
+        website: string;
+      };
+      handbook?: {
+        title: "Ou acesse o manual deste equipamento: ";
+        handbook: string[];
+      };
+    },
+  ];
 }
 
 export const equipamentFields: IEquipamentFields[] = [
   {
     title: "Arduino",
+    slug: "arduino",
     image: () => (
       <Image src={arduino} alt="arduino" className={equipamentStyle} />
     ),
@@ -20,61 +53,13 @@ export const equipamentFields: IEquipamentFields[] = [
                     código aberto amplamente utilizada por entusiastas,
                     hobistas, estudantes e profissionais da área de eletrônica e
                     programação.`,
-    href: "/equipamentos/arduino",
-  },
-  {
-    title: "Arduino",
-    image: () => (
-      <Image src={arduino} alt="arduino" className={equipamentStyle} />
-    ),
-    description: `O Arduino é uma plataforma de prototipagem eletrônica de
-                    código aberto amplamente utilizada por entusiastas,
-                    hobistas, estudantes e profissionais da área de eletrônica e
-                    programação.`,
-    href: "/equipamentos/arduino",
-  },
-  {
-    title: "Arduino",
-    image: () => (
-      <Image src={arduino} alt="arduino" className={equipamentStyle} />
-    ),
-    description: `O Arduino é uma plataforma de prototipagem eletrônica de
-                    código aberto amplamente utilizada por entusiastas,
-                    hobistas, estudantes e profissionais da área de eletrônica e
-                    programação.`,
-    href: "/equipamentos/arduino",
-  },
-  {
-    title: "Arduino",
-    image: () => (
-      <Image src={arduino} alt="arduino" className={equipamentStyle} />
-    ),
-    description: `O Arduino é uma plataforma de prototipagem eletrônica de
-                    código aberto amplamente utilizada por entusiastas,
-                    hobistas, estudantes e profissionais da área de eletrônica e
-                    programação.`,
-    href: "/equipamentos/arduino",
-  },
-  {
-    title: "Arduino",
-    image: () => (
-      <Image src={arduino} alt="arduino" className={equipamentStyle} />
-    ),
-    description: `O Arduino é uma plataforma de prototipagem eletrônica de
-                    código aberto amplamente utilizada por entusiastas,
-                    hobistas, estudantes e profissionais da área de eletrônica e
-                    programação.`,
-    href: "/equipamentos/arduino",
-  },
-  {
-    title: "Arduino",
-    image: () => (
-      <Image src={arduino} alt="arduino" className={equipamentStyle} />
-    ),
-    description: `O Arduino é uma plataforma de prototipagem eletrônica de
-                    código aberto amplamente utilizada por entusiastas,
-                    hobistas, estudantes e profissionais da área de eletrônica e
-                    programação.`,
-    href: "/equipamentos/arduino",
+    content: [
+      {
+        brand: {
+          title: "Marca: ",
+          brand: "Arduino",
+        },
+      },
+    ],
   },
 ];
