ca559d986a10595c1e2388f2c4456c3d0f02ecbf
Kyoudan <fogoste@gmail.com>
2023-08-02 12:14:36 -0300

    feat: adding accordion in resourcesFields

--- src/components/pages/Equipamentos/resources/index.tsx
+++ src/components/pages/Equipamentos/resources/index.tsx
@@ -1,39 +1,17 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { resourcesFields } from "@/mocks/equipaments/resources";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export const ResourcesEquipamentos = () => {
-  const [width, setWidth] = useState<number>();
-  const [isShow, setIsShow] = useState<"h-[300px]" | "">("");
-  const [isShowResource, setIsShowResource] = useState<"hidden" | "">();
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-    if ((width && width < 640) || window.innerWidth < 640) {
-      setIsShow("h-[300px]");
-      setIsShowResource("hidden");
-    } else {
-      setIsShow("");
-      setIsShowResource("");
-    }
-  }, []);
-
   return (
     <section
-      className={`bg-zinc-100 w-full flex items-center justify-center flex-col gap-4 p-8 relative overflow-hidden ${isShow}`}
+      className={`bg-zinc-100 w-full flex items-center justify-center flex-col gap-4 p-8 relative overflow-hidden`}
     >
       <div className="w-full flex items-center justify-center ">
         <h1 className="bg-[#cbfcbb] text-primary pt-1 pb-1 pr-3 pl-3 mt-2 rounded-full text-xl font-ubuntu border border-spacing-1 border-primary tracking-tighter uppercase">
@@ -41,14 +19,12 @@ export const ResourcesEquipamentos = () => {
         </h1>
       </div>
       <div>
-        <div className="w-full grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 flex-wrap sm:gap-4 gap-8  justify-around">
-          {resourcesFields.map((resource, index) => (
+        <div className="w-full lg:grid hidden md:grid-cols-3 sm:grid-cols-2 grid-cols-1 flex-wrap sm:gap-4 gap-8  justify-around">
+          {resourcesFields.map((resource) => (
             <>
               <div
                 key={resource.description}
-                className={`w-full flex xl:flex-row flex-col xl:text-start text-center gap-4 items-center hover:bg-primary-light/800 transition-colors rounded p-4 cursor-default sm:border-none border-b border-primary ${
-                  isShowResource && index > 1 ? isShowResource : ""
-                }`}
+                className={`w-full flex xl:flex-row flex-col xl:text-start text-center gap-4 items-center hover:bg-primary-light/800 transition-colors rounded p-4 cursor-default sm:border-none border-b border-primary `}
               >
                 <resource.icon />
                 <p className="sm:text-2xl text-xl font-montserrat">
@@ -58,34 +34,28 @@ export const ResourcesEquipamentos = () => {
             </>
           ))}
         </div>
-        {isShow == "h-[300px]" && (
-          <div className="w-full bg-zinc-200 absolute left-0 bottom-0 p-4 flex items-center justify-center shadow-zinc-200 shadow-3xl">
-            {" "}
-            <button
-              className="text-white font-montserrat font-bold text-2xl bg-primary transition-colors hover:bg-primary-light-200 p-2 rounded"
-              onClick={() => {
-                setIsShow("");
-                setIsShowResource("");
-              }}
-            >
-              Ver mais...
-            </button>
-          </div>
-        )}
-        {!isShow && width && width < 640 && (
-          <div className="w-full-light/400 absolute left-0 bottom-0 p-2 flex items-center justify-center">
-            {" "}
-            <button
-              className="text-white font-montserrat font-bold text-xl bg-primary transition-colors hover:bg-primary-light-200 p-2 rounded"
-              onClick={() => {
-                setIsShow("h-[300px]");
-                setIsShowResource("hidden");
-              }}
-            >
-              Esconder
-            </button>
-          </div>
-        )}
+
+        <div className="lg:hidden block transition-all">
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="w-[90%] sm:text-2xl text-xl font-montserrat bg-primary p-2 rounded font-bold text-white px-4">
+                Visualizar recursos disponiveis:
+              </AccordionTrigger>
+              <AccordionContent className=" flex flex-col">
+                {resourcesFields.map((resource) => (
+                  <>
+                    <div className=" flex flex-row gap-2 my-6 justify-start items-center">
+                      <resource.icon />
+                      <p className="sm:text-2xl text-xl font-montserrat">
+                        {resource.description}
+                      </p>
+                    </div>
+                  </>
+                ))}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </div>
     </section>
   );
