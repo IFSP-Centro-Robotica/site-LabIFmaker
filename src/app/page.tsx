72e4dcc37859398953564cfc8312b5b5c501766f
Martins-pedro23 <martinsdeoliveira.pedro2@gmail.com>
2023-08-01 21:26:51 -0300

    feat: fixing error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+'use client';
 import { FooterComponent } from "@/components/global/Footer";
 import { HeaderComponent } from "@/components/global/Header";
 import { AboutHome } from "@/components/pages/Home/About";
 import { OpeningHome } from "@/components/pages/Home/Opening";
 import { ScheduleHome } from "@/components/pages/Home/Schedule";
 import { SliderHome } from "@/components/pages/Home/Slider";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- src/components/pages/Home/Slider/index.tsx
+++ src/components/pages/Home/Slider/index.tsx
@@ -8,15 +8,31 @@ import bannerSmall2 from "@/assets/slidesHome/banner-small2.png";
 import banner1 from "@/assets/slidesHome/banner1.png";
 import banner2 from "@/assets/slidesHome/banner2.png";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "./styles.css";
 
-const hasWindow = typeof window !== "undefined";
-const windowWidth = hasWindow ? window.innerWidth : 0;
-
 export const SliderHome = () => {
+  const [width, setWidth] = useState<number>(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+  
   return (
     <div className="w-screen  overflow-x-hidden flex items-center justify-center bg-zin-300">
       <div className="w-[95%] pb-12 pt-12 border-spacing-1 ">
@@ -28,14 +44,14 @@ export const SliderHome = () => {
         >
           <SwiperSlide className="hidden">
             <Image
-              src={windowWidth > 768 ? banner1 : bannerSmall1}
+              src={width > 768 ? banner1 : bannerSmall1}
               alt="um banner"
               className="object-cover hidden"
             ></Image>
           </SwiperSlide>
           <SwiperSlide>
             <Image
-              src={windowWidth > 768 ? banner2 : bannerSmall2}
+              src={width > 768 ? banner2 : bannerSmall2}
               alt="um banner"
               className="object-cover"
             ></Image>
