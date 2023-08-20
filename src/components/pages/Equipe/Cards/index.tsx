ba89a4c696cf22452e14d6686d483baedb9c2e46
Martins-pedro23 <martinsdeoliveira.pedro2@gmail.com>
2023-08-20 19:57:24 -0300

    feat: changing equipe card

--- src/components/pages/Equipe/Cards/index.tsx
+++ src/components/pages/Equipe/Cards/index.tsx
@@ -10,7 +10,7 @@ interface IProps {
 export const CardComponentEquipe = ({ props }: IProps) => {
   return (
     <section className="w-full flex items-center justify-center flex-row gap-4 ">
-      <div className="sm:w-[80%] w-[90%] sm:h-[250px] flex sm:items-start items-center justify-start sm:flex-row flex-col gap-4 shadow-4xl  rounded p-3">
+      <div className="sm:w-[40%] w-[90%] sm:h-[250px] flex sm:items-start items-center justify-start sm:flex-row flex-col gap-4 shadow-4xl  rounded p-3">
         <Image src={props.image} alt="image" className="w-auto h-[100%]" />
         <div className="w-full h-full flex-col sm:items-start items-center flex gap-3 justify-between py-2  ">
           <p className="font-montserrat font-bold text-xl">{props.name}</p>
