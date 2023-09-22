c169465e3010510ed58a3ce20e7e38c546e7acbe
Kyoudan <fogoste@gmail.com>
2023-09-22 11:56:00 -0300

    feat: adding new menu

--- src/components/global/Header/index.tsx
+++ src/components/global/Header/index.tsx
@@ -4,20 +4,79 @@ import LogoIF from "@/assets/IfLogo.png";
 import Image from "next/image";
 import Link from "next/link";
 import { MenuPhoneComponent } from "../MenuPhone";
+import Icons from "@/assets/labifmakerlogosmall.svg";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import {
+  Home,
+  Hammer,
+  Image as ImageIcon,
+  GraduationCap,
+  Users2,
+  Bot,
+  ExternalLink,
+} from "lucide-react";
 
-const buttonsStyle =
-  "p-2 hover:bg-primary-light-100 transition-colors text-white rounded font-montserrat font-bold";
+
+const paths = [
+  {
+    name: "Inicio",
+    path: "/",
+    icon: () => <Home />,
+    title: "Pagina inicial",
+  },
+  {
+    name: "Equipamentos",
+    path: "/equipamentos",
+    icon: () => <Hammer />,
+    title: "Pagina de equipamentos",
+  },
+  {
+    name: "Galeria",
+    path: "/galeria",
+    icon: () => <ImageIcon />,
+    title: "Galeria",
+  },
+  {
+    name: "Cursos",
+    path: "/cursos",
+    icon: () => <GraduationCap />,
+    title: "Pagine de cursos",
+  },
+  {
+    name: "Projetos",
+    path: "/projetos",
+    icon: () => <Bot />,
+    title: "Pagina de projetos",
+  },
+  {
+    name: "Equipe",
+    path: "/equipe",
+    icon: () => <Users2 />,
+    title: "Pagina de equipe",
+  },
+];
+
+const linksStyle =
+  "w-full  transition-all hover:bg-zinc-200 hover:rounded font-montserrat font-bold p-2 flex items-center justify-start flex-col";
 
 export const HeaderComponent = () => {
   return (
-    <div className="w-full bg-primary flex sm:flex-row flex-col items-center justify-between px-4 pt-2 pb-2 gap-6">
-      <div className="lg:w-1/2 w-full flex flex-row justify-between lg:p-0 px-6">
+    <div className="w-full bg-primary flex sm:flex-row flex-col justify-center items-center  px-4 pt-2 pb-2 gap-6">
+      <div className="lg:w-1/2 w-full flex flex-row lg:justify-center justify-start lg:p-0 px-6">
         <Link href="/">
           <Image src={LogoIF} alt="logo" className="w-auto h-12" />
         </Link>
         <MenuPhoneComponent />
       </div>
-      <div className="sm:w-1/2 w-full flex-row items-center sm:justify-end justify-center gap-4 sm:mr-4 flex-wrap lg:flex hidden">
+      {/* <div className="sm:w-1/2 w-full flex-row items-center sm:justify-end justify-center gap-4 sm:mr-4 flex-wrap lg:flex hidden">
         <Link href="/" className={buttonsStyle}>
           Inicio
         </Link>
@@ -43,6 +102,81 @@ export const HeaderComponent = () => {
         >
           Agendamento
         </a>
+      </div> */}
+      <div className="sm:w-1/2 w-full flex-row items-center sm:justify-center justify-center gap-4 sm:mr-4 flex-wrap lg:flex hidden z-50">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="bg-primary-light-100 hover:bg-primary-light-100 font-montserrat font-semibold text-lg text-white">
+                Navegação
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                  <li className={`row-span-6`}>
+                    <NavigationMenuLink asChild>
+                      <a
+                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-xl bg-zinc-500"
+                        href="/"
+                      >
+                        <Image
+                          alt="Logo labif"
+                          src={Icons}
+                          className="h-24 w-24"
+                        />
+                        <div className="mb-2 mt-4 text-lg font-medium">
+                          Laboratorio maker
+                        </div>
+                        <p className="text-sm leading-tight text-muted-foreground">
+                          Navegue em nosso site clicando nos links ao lado
+                        </p>
+                      </a>
+                    </NavigationMenuLink>
+                  </li>
+                  {paths.map((item, index) => (
+                    <div className="w-full flex flex-col" key={`nav-link-map-${index}`}>
+                      <Link
+                        href={item.path}
+                        title={item.title}
+                        className={linksStyle}
+                      >
+                        <div className="w-full flex flex-row gap-6">
+                          <item.icon />
+                          {item.name}
+                        </div>
+                      </Link>
+                      {index != paths.length && (
+                        <span className="w-full h-[1px]  border-b border-zinc-400 my-1"></span>
+                      )}
+                    </div>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="bg-primary-light-100 hover:bg-primary-light-100 font-montserrat font-semibold text-lg text-white">
+                Outros
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                  <h1 className={linksStyle}>Documentação</h1>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem >
+              <Link
+                href="https://docs.google.com/forms/d/e/1FAIpQLSdGAPut5VFQ6uNyNjfLxX-LiTJvQuL3a4ciRPAOb66dJcvBlQ/viewform?usp=sf_link"
+                legacyBehavior
+                passHref
+                
+              >
+                <NavigationMenuLink className={navigationMenuTriggerStyle() + " bg-primary-light-100 hover:bg-primary-light-100 font-montserrat font-semibold text-lg text-white flex flex-row gap-2"} >
+                  <ExternalLink />
+                  Agendamento
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
     </div>
   );
--- src/components/pages/Home/About/Description/index.tsx
+++ src/components/pages/Home/About/Description/index.tsx
@@ -1,21 +1,42 @@
+"use client";
+
 import { ArchiveRestore, Lightbulb, Wrench } from "lucide-react";
+import {
+  motion,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { useRef } from "react";
 
 export const Description = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 0", "2 2"],
+  });
+  const scaleProgress = useTransform(scrollYProgress, [1, 0], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.9, 1]);
+
+
   return (
     <>
-      <div className="w-full flex flex-col gap-8">
-        <h1 className="font-montserrat font-bold sm:text-6xl text-5xl">
+      <motion.div
+        style={{ scale: scaleProgress, opacity: opacityProgress }}
+        className="w-full flex flex-col gap-8"
+        ref={ref}
+      >
+        <h1   className="font-montserrat font-bold sm:text-6xl text-5xl">
           O que é o Lab
           <span className="text-primary">IF</span> Maker?
         </h1>
-        <p className="font-montserrat text-xl text-justify hyphens-auto">
+        <p  className="font-montserrat text-xl text-justify hyphens-auto">
           O LabIF Maker é um espaço de produção criativa, onde os alunos, os
           servidores e a comunidade externa podem desenvolver projetos de forma
           colaborativa, utilizando ferramentas de fabricação digital, como
           impressoras 3D, cortadoras a laser, entre outras. O espaço é aberto a
           todos, basta realizar o seu agendamento!
         </p>
-      </div>
+      </motion.div>
       <div className="w-full flex flex-col gap-12">
         <div className="w-full flex sm:flex-row flex-col sm:border-none border-b pb-8 border-spacing-1 border-black gap-4 items-center justify-center">
           <div className="rounded-[50%] border border-spacing-1 border-zinc-400 flex items-center justify-center p-4 hover:bg-zinc-200 transition-colors">
@@ -31,9 +52,9 @@ export const Description = () => {
             <ArchiveRestore size={60} className="text-primary " />
           </div>
           <p className="font-montserrat text-xl text-justify hyphens-auto">
-            O laboratório disponibiliza todos os materiais ne&shy;ce&shy;ssários para a
-            pro&shy;dução dos projetos. após a con&shy;clusão do projeto, este poderá ser
-            levado para casa.{" "}
+            O laboratório disponibiliza todos os materiais ne&shy;ce&shy;ssários
+            para a pro&shy;dução dos projetos. após a con&shy;clusão do projeto,
+            este poderá ser levado para casa.{" "}
           </p>
         </div>
         <div className="w-full flex sm:flex-row flex-col  pb-8 border-spacing-1 border-black gap-4 items-center justify-center">
@@ -41,9 +62,9 @@ export const Description = () => {
             <Wrench size={60} className="text-primary " />
           </div>
           <p className="font-montserrat text-xl text-justify hyphens-auto">
-            O LabIF Maker dispo&shy;nibiliza equi&shy;pamen&shy;tos mo&shy;dernos e de alta
-            qualidade para a produção dos projetos. Para que estes te&shy;nham a
-            melhor qua&shy;lidade possível!{" "}
+            O LabIF Maker dispo&shy;nibiliza equi&shy;pamen&shy;tos
+            mo&shy;dernos e de alta qualidade para a produção dos projetos. Para
+            que estes te&shy;nham a melhor qua&shy;lidade possível!{" "}
           </p>
         </div>
       </div>
